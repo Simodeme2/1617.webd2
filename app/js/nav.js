4152e6fdc7c43d978b27920e7298f7d71aa35996
Simodeme2 <Simodeme2@student.arteveldehs.be>
2017-05-27 17:22:37 +0200

    Website

---
+++ app/js/nav.js
@@ -0,0 +1,27 @@
+var button = document.querySelector(".hamburger");
+var nav = document.querySelectorAll(".column-4");
+var count = 0;
+
+button.addEventListener("click", openAndClose);
+
+function openAndClose() {
+
+    if (count % 2 < 1) {
+        button.src = "../assets/close.svg";
+
+        for (i = 0; i < nav.length; ++i) {
+            nav[i].style.display = "block";
+        }
+        count++;
+
+
+    } else {
+        button.src = "../assets/nav.svg";
+
+        for (i = 0; i < nav.length; ++i) {
+            nav[i].style.display = "none";
+        }
+        count++;
+
+    }
+}
